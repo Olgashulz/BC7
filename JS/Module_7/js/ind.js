f026d98e0e6a94dae28b80a2df06a4ed499317e2
Olgashulz <oll.cat4@gmail.com>
2018-09-18 08:57:16 +0300

    added module 7

--- JS/Module_7/js/ind.js
+++ JS/Module_7/js/ind.js
@@ -10,7 +10,75 @@
     массив DOM-элементов всех постов.
 
   3. Повесьте все посты в какой-то уже существующий DOM-узел.*/
+/*
+function createPostCard(post) {
+//create element
+    let root = document.querySelector('#root');
+    let post = document.createElement('div');
+    let postImage = document.createElement('img');
+    let postTitle = document.createElement('h2');
+    let postText = document.createElement('p');
+    let button = document.createElement('a');
 
+//add atributes
+    postImage.setAttribute('src', 'http://via.placeholder.com/400x150');
+    postImage.setAttribute('alt', 'post image');
+    button.setAttribute('href', '#');
+
+// add classList
+    post.classList.add('post');
+    post.classList.add('post__image');
+    postTitle.classList.add('post__title');
+    postText.classList.add('post__text');
+    button.classList.add('button');
+
+//text content
+    postTitle.textContent = 'Lorem ipsum dolor';
+    postText.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!';
+    button.textContent = 'Read more';
+
+//paste on page
+    root.prepend(post);
+    post.prepend(postImage);
+    postImage.after(postTitle);
+    postTitle.after(postText);
+    postText.after(button);
+
+}
+createPostCard();*!/*/
+
+/*
+let createPostCard = function (poster) {
+
+for (let i of posts) {
+    let root = document.querySelector('#root');
+    let post = document.createElement('div');
+    let postImage = document.createElement('img');
+    let postTitle = document.createElement('h2');
+    let postText = document.createElement('p');
+    let button = document.createElement('a');
+
+    postImage.setAttribute('src', i.img);
+    postImage.setAttribute('alt', 'post image');
+    button.setAttribute('href', i.link);
+
+    post.classList.add('post');
+    postImage.classList.add('post__image');
+    postTitle.classList.add('post__title');
+    postText.classList.add('post__text');
+    button.classList.add('button');
+
+    postTitle.textContent = i.title;
+    postText.textContent = i.text;
+    button.textContent = 'read more';
+
+    root.prepend(post);
+    post.prepend(postImage);
+    postImage.after(postTitle);
+    postTitle.after(postText);
+    postText.after(button);
+}};
+console.log(createPostCard(posts));*/
 
 const posts = [
     {
@@ -33,12 +101,9 @@ const posts = [
     }
 ];
 
-
 let root = document.querySelector('#root');
-let createPostCard = function (poster) {
 
-for (let i of posts) {
-    let root = document.querySelector('#root');
+let createPostCard = function (i) {
     let post = document.createElement('div');
     let postImage = document.createElement('img');
     let postTitle = document.createElement('h2');
@@ -64,7 +129,18 @@ for (let i of posts) {
     postImage.after(postTitle);
     postTitle.after(postText);
     postText.after(button);
-    root.append(post);
-}};
-console.log(createPostCard(posts));
+
+};
+createPostCard(posts);
+
+
+//root.append(post);
+let finalPost ;
+
+function createCards(posts){
+    finalPost =posts.map(function(el){
+        createCards(posts);
+    });
+    root.append(finalPost);
+}
 
