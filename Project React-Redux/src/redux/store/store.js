fd5c41e231e0c6520e91345af324d4e51b35b287
Olgashulz <oll.cat4@gmail.com>
2018-11-19 10:13:38 +0200

    changed store

--- Project React-Redux/src/redux/store/store.js
+++ Project React-Redux/src/redux/store/store.js
@@ -1,17 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import  rootReducer from '../reducers';
 import thunk from 'redux-thunk';
-import {createBrowserHistory } from 'history';
-import {connectRouter} from 'connected-react-router'
-
-import rootReducer from "../reducers";
+import { createBrowserHistory } from 'history';
+import { connectRouter } from 'connected-react-router'
 
 export const history = createBrowserHistory();
-const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-
-const store = createStore(connectRouter(history)(rootReducer),
-    compose(applyMiddleware(thunk), DevTools));
-
-
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore (connectRouter(history)(rootReducer), composeEnhancers(applyMiddleware(thunk)));
 export default store;
+
